b3cf263dbf59edcecc8042315f06a2574a89307d
Markus Rossio <argitavis@gmail.com>
2022-02-22 16:29:09 -0600

    Add routes wraper to app js, Add route to default route element h1 with text home should be changed to actual component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Routes>
+      <Route path='/' element={<h1>Home</h1>}/>
+    </Routes>
   );
 }
 
