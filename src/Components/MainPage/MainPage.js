4b0a44eb73e50be4bc52c680b0d5dddd88baa9dc
Markus Rossio <argitavis@gmail.com>
2022-02-22 18:24:54 -0600

    Add state update with articleresults on page load for home

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './MainPage.css';
+import { getStories } from "../../Services/API/api";
 import MainPageHeader from "../MainPageHeader/MainPageHeader";
 import MainPageSection from "../MainPageSection/MainPageSection";
 
 export default function MainPage() {
   const [filterOption, setFilterOption] = useState('');
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    getStories('home')
+      .then(data => setArticles(data.results));
+  }, []);
 
   return (
     <>
