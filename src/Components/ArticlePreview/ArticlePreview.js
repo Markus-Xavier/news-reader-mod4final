d9eb221a82e34c6731cc5ab7bea780473273aa67
Markus Rossio <argitavis@gmail.com>
2022-02-22 21:18:35 -0600

    Learn to spell multimedia

--- src/Components/ArticlePreview/ArticlePreview.js
+++ src/Components/ArticlePreview/ArticlePreview.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './ArticlePreview.css';
 
 export default function ArticlePreview({ title, publishDate, writtenBy, previewImage }) {
+  console.log(title)
   return (
     <h1>Dog Boy</h1>
   )
--- src/Components/MainPageSection/MainPageSection.js
+++ src/Components/MainPageSection/MainPageSection.js
@@ -3,11 +3,19 @@ import './MainPageSection.css';
 import ArticlePreview from "../ArticlePreview/ArticlePreview";
 
 export default function MainPageSection({ sectionTitle, articles }) {
-  const createArticlePreviews = () => {
-    
-  }
+  let articlePreviews = '';
+
+  articlePreviews = articles.map(article => <ArticlePreview
+    title={article.title} 
+    publishDate={article.publish_date}
+    writtenBy={article.byline}
+    previewImage={article.multimedia[2]}
+    />);
 
   return(
-    <h1>{sectionTitle}</h1>
+    <>
+      <h1>{sectionTitle}</h1>
+      {articlePreviews}
+    </>
   )
 };
