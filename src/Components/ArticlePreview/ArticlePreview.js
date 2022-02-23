36052d7db769f9dfa9933c20912b440a57d20db8
Markus Rossio <argitavis@gmail.com>
2022-02-22 20:50:57 -0600

    Create articlepreview componenent, Add articles prop to mainpagesection, Add function createartcilepreviews to mainpagesection to handle articlepreview render

---
+++ src/Components/ArticlePreview/ArticlePreview.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ArticlePreview.css';
+
+export default function ArticlePreview({ title, publishDate, writtenBy, previewImage }) {
+  return (
+    <h1>Dog Boy</h1>
+  )
+}
--- src/Components/MainPageSection/MainPageSection.js
+++ src/Components/MainPageSection/MainPageSection.js
@@ -1,7 +1,12 @@
 import React from "react";
 import './MainPageSection.css';
+import ArticlePreview from "../ArticlePreview/ArticlePreview";
 
-export default function MainPageSection({ sectionTitle }) {
+export default function MainPageSection({ sectionTitle, articles }) {
+  const createArticlePreviews = () => {
+
+  }
+
   return(
     <h1>{sectionTitle}</h1>
   )
