a28f311597f0d7e7e0d5e7a35b0b257b84c9eb16
Markus Rossio <argitavis@gmail.com>
2022-02-22 18:18:40 -0600

    Create mainpagesection component and import into mainpage js

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import './MainPage.css';
 import MainPageHeader from "../MainPageHeader/MainPageHeader";
+import MainPageSection from "../MainPageSection/MainPageSection";
 
 export default function MainPage() {
   const [filterOption, setFilterOption] = useState('');
 
   return (
-    <MainPageHeader filterOption={filterOption}/>
+    <>
+      <MainPageHeader filterOption={filterOption}/>
+    </>
   )
 }
