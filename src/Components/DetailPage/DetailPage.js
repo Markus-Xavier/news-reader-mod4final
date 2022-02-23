70ca136095520e494767061e2c895451a09c6e24
Markus Rossio <argitavis@gmail.com>
2022-02-22 22:16:40 -0600

    Add getting url params from details page to retreive artical id and category

--- src/Components/DetailPage/DetailPage.js
+++ src/Components/DetailPage/DetailPage.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import'./DetailPage.css';
 
 export default function DetailPage() {
+  const { id, category } = useParams();
   return (
     <>
       <h1>Hi Detail Page</h1>
