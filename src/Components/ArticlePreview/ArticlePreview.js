5308599fcffa32da797368ad66fbbfc30d2739b5
Markus Rossio <argitavis@gmail.com>
2022-02-23 01:30:45 -0600

    Add basic layout to main page

--- src/Components/ArticlePreview/ArticlePreview.js
+++ src/Components/ArticlePreview/ArticlePreview.js
@@ -5,12 +5,16 @@ import './ArticlePreview.css';
 export default function ArticlePreview({ id, title, publishDate, writtenBy, previewImage, filterOption }) {
   return (
     <Link to={`/${filterOption}/${id}`}>
-      <>
-        <p>{title}</p>
-        <p>{writtenBy}</p>
-        <p>{publishDate}</p>
+      <div className="article-preview_display">
         <img src={previewImage.url}/>
-      </>
+        <div>
+          <p className="article-preview_title">{title}</p>
+          <div className="article-preview_info-display">
+            <p>{writtenBy}</p>
+            <p>{publishDate}</p>
+          </div>
+        </div>
+      </div>
     </Link>
   )
 }
